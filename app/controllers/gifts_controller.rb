3e1333f3e6c74db62f98e23315dd2f82bff4e4ad
Yohan Gouiran <y.gouiran@gmail.com>
2024-03-26 15:41:46 +0100

    openai ok - creation event + gifts ok - gift puis event ok

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,22 +1,41 @@
 class GiftsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_client, only: [:create]
 
   def index
     @gifts = Gift.all
   end
 
   def show
+    @event = params[:event_id] ? Event.find(params[:event_id]) : Event.new
     @gift = Gift.find(params[:id])
   end
 
   def new
+    @event = params[:event_id] ? Event.find(params[:event_id]) : Event.new
     @gift = Gift.new
+    @gift.event = @event
   end
 
   def create
+    @event = params[:event_id] ? Event.find(params[:event_id]) : Event.new
     @gift = Gift.new(gift_params)
+    @gift.event = @event
     @gift.user = current_user
-    if @gift.save!
+    @gift.interests = @gift.interests.compact_blank
+    @gift.generated_list = @gift.gen_gifts($client, @gift.budget, @gift.age, @gift.genre, @gift.occasion, @gift.interests, @gift.relationship).split(/\d+\.\s+/).map(&:strip).compact_blank
+    if @gift.save
+      redirect_to gift_path(@gift, @event)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @gift = Gift.find(params[:id])
+    comment = params[:comment]
+    @gift.update(generated_list: @gift.update_gifts($client, comment, @gift.interests).split(/\d+\.\s+/).map(&:strip).compact_blank)
+    if @gift.save
       redirect_to gift_path(@gift)
     else
       render :new
@@ -26,7 +45,11 @@ class GiftsController < ApplicationController
   private
 
   def gift_params
-    params.require(:gift).permit(:budget, :age, :genre, :occasion, [interests: []], :relationship, :generated_list, :user_id, :event_id)
+    params.require(:gift).permit(:budget, :age, :genre, :occasion, [interests: []], :relationship, :generated_list, :comment, :user_id, :event_id)
+  end
+
+  def set_client
+    $client = $client ? $client : OpenAI::Client.new
   end
 
 end
