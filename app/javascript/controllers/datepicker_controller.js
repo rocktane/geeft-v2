402cd3a83dcc6c02058539b7c9ec3b22e12366d7
Yohan Gouiran <y.gouiran@gmail.com>
2024-04-11 18:02:49 +0200

    interactions on dashboard to check

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,29 +1,69 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
+import "flatpickr/dist/l10n/fr.js";
 
 export default class extends Controller {
   connect() {
+    const userLang = navigator.language === "fr" ? "fr" : "en";
+
     const booked = JSON.parse(
       document.body
         .querySelector("[data-events-dates]")
         .getAttribute("data-events-dates")
     );
-    console.log(booked);
-    console.log(booked.length);
-    console.log(typeof booked);
 
-    flatpickr(this.element, {
+    const calendar = flatpickr(this.element, {
+      locale: userLang,
+      inline: "true",
+      altInput: true,
+      altFormat: "F j, Y",
+      dateFormat: "Y-m-d",
+
+      // onChange: function (selectedDates, dateStr, instance) {
+      //   if (
+      //     document.querySelector(".dayContainer").querySelector(".selected")
+      //   ) {
+      //     const selected_date = document
+      //       .querySelector(".dayContainer")
+      //       .querySelector(".selected");
+      //     selected_date.setAttribute("data-selected-date", dateStr);
+      //   }
+      // },
+
+      onChange: function (selectedDates, dateStr, instance) {
+        const event = new CustomEvent("dateChange", {
+          detail: { selectedDates, dateStr, instance },
+        });
+        document.dispatchEvent(event);
+      },
+
       onDayCreate: function (dObj, dStr, fp, dayElem) {
-        let date = dayElem.dateObj.toISOString(true).slice(0, 10).valueOf();
+        const offset = dayElem.dateObj.getTimezoneOffset() * 60000;
+        const adjustedDate = new Date(dayElem.dateObj.getTime() - offset);
+        const isoString = adjustedDate.toISOString().slice(0, 10).valueOf();
 
         booked.forEach((bookedDate) => {
-          console.log("boucle");
-          if (date == bookedDate) {
-            console.log("booked found");
+          if (isoString == bookedDate) {
             dayElem.innerHTML += "<span class='booked'></span>";
           }
         });
       },
+
+      onMonthChange: function () {
+        document
+          .querySelector(".flatpickr-current-month")
+          .setAttribute("data-month", calendar.currentMonth);
+        document
+          .querySelector(".flatpickr-current-month")
+          .setAttribute("data-year", calendar.currentYear);
+      },
     });
+
+    document
+      .querySelector(".flatpickr-current-month")
+      .setAttribute("data-month", calendar.currentMonth);
+    document
+      .querySelector(".flatpickr-current-month")
+      .setAttribute("data-year", calendar.currentYear);
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
+pin "flatpickr/dist/l10n/fr.js", to: "flatpickr--dist--l10n--fr.js.js" # @4.6.13
