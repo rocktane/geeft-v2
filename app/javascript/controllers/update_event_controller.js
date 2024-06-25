f4f0fd7a7fc1042e28d9bd0b55f08a2f6669b54c
Yohan Gouiran <y.gouiran@gmail.com>
2024-06-25 16:20:30 +0200

    update event ok sauf la date

---
+++ app/javascript/controllers/update_event_controller.js
@@ -0,0 +1,67 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["gift", "name", "description", "url", "date"];
+
+  connect() {
+    console.log("name", this.nameTarget.value);
+    console.log("description", this.descriptionTarget.value);
+    console.log("url", this.urlTarget.value);
+    console.log("date", this.dateTarget.value);
+  }
+
+  async update(event) {
+    event.preventDefault();
+
+    let env = event.currentTarget.dataset.env || "";
+    const eventId = document
+      .querySelector("[data-event-id]")
+      .getAttribute("data-event-id");
+
+    // If the environment is not defined, we set it to development
+    if (env === "development") {
+      env = "http://localhost:3000";
+    } else {
+      env = "https://www.geeft.club";
+    }
+
+    const url = `${env}/events/${eventId}`;
+    const newList = this.giftTargets.map((gift) => gift.innerText);
+    console.log("newList", newList);
+
+    // const url = `${env}/updatelist/${giftId}`;
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+    try {
+      const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-CSRF-Token": csrfToken, // Incluez le jeton CSRF dans les en-têtes
+        },
+        body: JSON.stringify({
+          gift_list: newList,
+          name: this.nameTarget.value,
+          url: this.urlTarget.value,
+          description: this.descriptionTarget.value,
+        }), // A ADAPTER POUR LE generated_list au cas ou Gift
+      });
+
+      const data = await response.json();
+      console.log("data", data);
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Error data:", errorData);
+        throw new Error("Network response was not ok");
+      }
+
+      window.setTimeout(() => {
+        window.location.href = url;
+      }, 500);
+    } catch (error) {
+      console.error("Une erreur s'est produite : ", error);
+    }
+  }
+}
