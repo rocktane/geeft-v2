10e692795bf8c08ff62599d1920431776e3b9f58
Yohan Gouiran <y.gouiran@gmail.com>
2024-07-03 16:38:02 +0200

    js search fixed

--- app/javascript/controllers/search_input_controller.js
+++ app/javascript/controllers/search_input_controller.js
@@ -2,7 +2,12 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search-input"
 export default class extends Controller {
-  static targets = ["input"];
+  static targets = [
+    "input",
+    "relationshipInput",
+    "occasionInput",
+    "interestsInput",
+  ];
   connect() {}
 
   animate(event) {
@@ -16,6 +21,7 @@ export default class extends Controller {
 
     searchInput.style.border = "1px solid #ccc";
     if (searchInput.style.width === "0px" || searchInput.style.width === "") {
+      searchInput.focus();
       // Measure the final width by setting the width to 'auto' temporarily
       searchInput.style.width = "auto";
       searchInput.style.padding = "0.25em 0.5em 0.25em 2.5em";
@@ -27,7 +33,6 @@ export default class extends Controller {
 
       setTimeout(function () {
         searchInput.style.padding = "0.25em 0.5em 0.25em 2.5em";
-        searchInput.focus();
         if (section === "interests") {
           xMark.style.opacity = "1";
           reset.style.opacity = "1";
@@ -42,10 +47,13 @@ export default class extends Controller {
       searchInput.style.width = finalWidth;
     } else {
       // Collapse the div back to 0 width
+      searchInput.value = "";
       searchInput.style.width = "0px";
       searchInput.style.padding = "0.25em 0em 0.25em 1em";
-      xMark.style.opacity = "0";
-      reset.style.opacity = "0";
+      if (section === "interests") {
+        xMark.style.opacity = "0";
+        reset.style.opacity = "0";
+      }
     }
   }
 
@@ -57,7 +65,9 @@ export default class extends Controller {
       .toUpperCase();
   }
 
-  filterList(event = "interests") {
+  filterList(section) {
+    this.deleteUnchecked();
+
     let input,
       filter,
       ul,
@@ -66,52 +76,59 @@ export default class extends Controller {
       atLeastOneVisible = false,
       span,
       sectionName;
-    if (event === "interests") {
-      sectionName = "interests";
-    } else {
-      sectionName =
-        event.target.parentElement.querySelector("span").dataset
-          .searchInputValue;
-    }
-    input = this.inputTarget;
+
+    sectionName =
+      event.target.parentElement.querySelector("span").dataset.searchInputValue;
+
+    input = this[sectionName + "InputTarget"];
     filter = this.normalizeText(input.value);
     ul = document.querySelector(`.gift_${sectionName}`);
     span = ul.getElementsByTagName("span");
-
-    if (document.querySelectorAll('[id^="new-gift-"]')) {
-      const newGifts = document.querySelectorAll('[id^="new-gift-"]');
-      newGifts.forEach((newGift) => {
-        if (newGift.parentElement.querySelector("input").checked === false) {
-          newGift.parentElement.remove();
-        }
-      });
-    }
 
     for (i = 0; i < span.length; i++) {
       txtValue = span[i].textContent || span[i].innerText;
       if (this.normalizeText(txtValue).indexOf(filter) > -1) {
         span[i].style.display = "";
-        if (sectionName === "interests") {
-          atLeastOneVisible = true;
-        }
+        atLeastOneVisible = true;
       } else {
         span[i].style.display = "none";
       }
     }
-    this.addIfNeeded(input.value, atLeastOneVisible);
+
+    if (sectionName === "interests" && !atLeastOneVisible) {
+      this.addIfNeeded(input.value);
+    }
   }
-  addIfNeeded(input, atLeastOneVisible) {
-    if (!atLeastOneVisible) {
-      const search = input.charAt(0).toUpperCase() + input.slice(1);
-      const searchLower = search.toLowerCase().replace(/\s/g, "");
-      const div = document.querySelector("div.gift_interests");
-      const content = `
-          <input class="check_boxes optional" type="checkbox" value="${search}" name="gift[interests][]" id="gift_interests_${searchLower}">
+
+  addIfNeeded(input) {
+    const search = input.charAt(0).toUpperCase() + input.slice(1);
+    const searchLower = search.toLowerCase().replace(/\s/g, "");
+    const div = document.querySelector("div.gift_interests");
+    const checkboxes = Array.from(
+      document.querySelectorAll("span[class='checkbox']")
+    );
+    const lastElement = checkboxes[checkboxes.length - 1];
+
+    console.log("lastElement", lastElement);
+    console.log("value last input", lastElement.querySelector("input").value);
+    console.log("searchLower", searchLower);
+
+    const content = `
+          <input class="check_boxes optional tempGift" type="checkbox" value="${search}" name="gift[interests][]" id="gift_interests_${searchLower}">
           <label class="collection_check_boxes" for="gift_interests_${searchLower}" id="new-gift-${searchLower}">
             <i class="fa-solid fa-square-plus"></i>
             ${search}
           </label>
         `;
+
+    const newCheckboxes = document.querySelectorAll(
+      ".interests span[class='tempGift'] input[type='checkbox']:not(:checked)"
+    );
+
+    if (newCheckboxes.length > 0) {
+      lastElement.innerHTML = content;
+      lastElement.style.display = "";
+    } else {
       const span = document.createElement("span");
       span.classList.add("checkbox");
       div.appendChild(span);
@@ -120,9 +137,9 @@ export default class extends Controller {
   }
 
   empty() {
-    this.inputTarget.value = "";
-    this.inputTarget.focus();
-    this.filterList();
+    this.interestsInputTarget.value = "";
+    this.interestsInputTarget.focus();
+    this.filterList("interests");
   }
 
   reset() {
@@ -141,23 +158,14 @@ export default class extends Controller {
     this.empty();
   }
 
-  // function empty() {
-  //   document.getElementById('searchInterestsInput').value = '';
-
-  // }
-
-  // function reset() {
-  //   this.empty();
-  //   const checkboxes = document.querySelectorAll('.interests input[type="checkbox"]:checked');
-  //   checkboxes.forEach((checkbox) => {
-  //     checkbox.checked = false;
-  //   });
-  //   if (document.querySelectorAll('[id^="new-gift-"]')) {
-  //     const newGifts = document.querySelectorAll('[id^="new-gift-"]');
-  //     newGifts.forEach((newGift) => {
-  //       newGift.parentElement.remove();
-  //     });
-  //   }
-  //   filterList('interests');
-  // }
+  deleteUnchecked() {
+    const checkboxes = document.querySelectorAll(
+      ".interests input[type='checkbox']:not(:checked)"
+    );
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.classList.contains("tempGift")) {
+        checkbox.parentElement.remove();
+      }
+    });
+  }
 }
