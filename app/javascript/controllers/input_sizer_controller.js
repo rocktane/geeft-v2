8dcd57879f4b965528110e2922b6e1836b287880
Nina Laouadi <nina.laouadi0@gmail.com>
2024-07-31 22:54:23 +0200

    js-edit ok

--- app/javascript/controllers/input_sizer_controller.js
+++ app/javascript/controllers/input_sizer_controller.js
@@ -1,19 +1,19 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["textarea"];
+  static targets = ["text"];
 
   connect() {
     console.log("InputSizerController connected");
-    this.textareaTargets.forEach((textarea) => {
-      this.adjustHeight(textarea);
-      textarea.addEventListener('input', () => this.adjustHeight(textarea));
+    this.textTargets.forEach((text) => {
+      this.adjustHeight(text);
+      text.addEventListener('input', () => this.adjustHeight(text));
     });
   }
 
-  adjustHeight(textarea) {
-    textarea.style.height = 'auto'; // Reset height to auto
-    textarea.style.height = `${textarea.scrollHeight}px`; // Set height to scrollHeight
-    console.log(`New height set to: ${textarea.style.height}`);
+  adjustHeight(text) {
+    text.style.height = 'auto';
+    text.style.height = `${text.scrollHeight}px`;
+    console.log(`New height set to: ${text.style.height}`);
   }
 }
